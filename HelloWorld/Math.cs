6e26306744911f6b0df1ca269ddadebb15a62237
chkno <chkno@LAPTOP-SJTAKC8S>
2023-11-10 19:07:00 +0100

    Add project files.

---
+++ HelloWorld/Math.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Text;
+
+
+namespace HelloWorld
+{
+    public class Math
+    {
+        public double Add(double a, double b)
+        {
+            return a + b;
+        }
+
+        public double Substract(double a, double b)
+        {
+            return a - b;
+        }
+    }
+}
